c3e9e299da7071cad2a470a8b144225e95c45e54
maderikra <6333132+maderikra@users.noreply.github.com>
2017-08-14 15:52:05 -0400

    formatting

--- overviewbreakout.php
+++ overviewbreakout.php
@@ -1,73 +1,52 @@
  <?php
-//
-//
 
-
-	if (isset($_GET['lcclass'])){ $lcclass = $_GET['lcclass']; }
-	else {	$lcclass = "";}
-	
-	
-//check if it's a single letter only (i.e., distinguish between all of B, and B1-B9999; the latter will be represneted as B0
-if ((strlen($lcclass) == 2) and ($lcclass[1] == '0'))
-{
-$lcclass = $lcclass[0] . "[0-9]";
+if (isset($_GET['lcclass'])) {
+    $lcclass = $_GET['lcclass'];
+} else {
+    $lcclass = "";
 }
 
-	
+//check if it's a single letter only (i.e., distinguish between all of B, and B1-B9999; the latter will be represneted as B0
+if ((strlen($lcclass) == 2) and ($lcclass[1] == '0')) {
+    $lcclass = $lcclass[0] . "[0-9]";
+}
+
 include "includes/dbconnect.php";
 
 include "includes/preparedates_single.php";
-	
 
 //insert link in each one to make chart clickable
-foreach  ($allyears as $k4 => $v4){
-	if ($v4['value'] <> ""){
-		//insert correspreond lc class to make link
-		$allyears[$k4]['link'] = "JavaScript:getresults('" . $v4['label'] . "','','" . $lcclass . "')";
-	}
+foreach ($allyears as $k4 => $v4) {
+    if ($v4['value'] <> "") {
+        //insert correspreond lc class to make link
+        $allyears[$k4]['link'] = "JavaScript:getresults('" . $v4['label'] . "','','" . $lcclass . "')";
+    }
 }
 
 $jsonresults = json_encode($allyears);
 
-
-
 //set page title here
 $pagetitle = "Total Items by Copyright Date";
 
-
-
 include "includes/header.php";
-
-
-
 
 ?>
 
 <script type="text/javascript">
-
 function getresults(year,series,lcclass){
 	//check if second character is lowercase d - if so, it's a first letter only class (B0)
-
-	$("#itemsgohere").load("results.php?year=" + year + "&series=" + series + "&lcclass=" + lcclass );
-	
+	$("#itemsgohere").load("results.php?year=" + year + "&series=" + series + "&lcclass=" + lcclass );	
 }
 
 $(document).ready(function(e) {
-
 var url = window.location.href;
 var matchvar = url.match(/\?lcclass=[A-Z]/gi);
 if(url.indexOf(matchvar) != -1){
-     $('input:radio[id=<?php echo $_GET['lcclass']; ?>]').prop('checked', true);
+     $('input:radio[id=<?php
+echo $_GET['lcclass'];
+?>]').prop('checked', true);
 }
-});
-
-
-
-
-
-
-		 
-		 
+});		 	 
 </script>
 
 <style type="text/css">
@@ -94,7 +73,9 @@ font-weight:normal;
 
   <body>
 
-<?php include 'includes/navbar_class.php';?>
+<?php
+include 'includes/navbar_class.php';
+?>
 
     <div class="container-fluid">
       <div class="row">
@@ -104,34 +85,42 @@ font-weight:normal;
       
 <div id="accordion">
 
-<?php include "includes/classmenu.php";?>
+<?php
+include "includes/classmenu.php";
+?>
 
 </div>
 </div>
 </form>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                 <?php include "includes/collapseleft.php" ?>  
-    <h2>Total Items by Copyright Date <?php include "includes/expanded.php" ?></h2>
+                 <?php
+include "includes/collapseleft.php";
+?>  
+    <h2>Total Items by Copyright Date <?php
+include "includes/expanded.php";
+?></h2>
     
    <?php
-   if ($lcclass == "") {$lcdisplay = "All Classes";}
-   elseif((strlen($lcclass) > 1) and ($lcclass[1] == '[')) {$lcdisplay = "LC Class: " . $lcclass[0] . "1 - " . $lcclass[0] . "9999";  }
-   else  {$lcdisplay = "LC Class: " . $lcclass;}
-   ?> 
-                        <h3><?php echo $lcdisplay; ?></h3>        
-
-
-
+if ($lcclass == "") {
+    $lcdisplay = "All Classes";
+} elseif ((strlen($lcclass) > 1) and ($lcclass[1] == '[')) {
+    $lcdisplay = "LC Class: " . $lcclass[0] . "1 - " . $lcclass[0] . "9999";
+} else {
+    $lcdisplay = "LC Class: " . $lcclass;
+}
+?> 
+                        <h3><?php
+echo $lcdisplay;
+?></h3>        
 
 <div id="chartholder">
 <canvas id="mainchart"></canvas>
 </div>
 <div id="itemsgohere"></div>
 
-
-
-<?php include "includes/chart_general.php"; 
+<?php
+include "includes/chart_general.php";
 display_chart("Copyright Date");
 
 ?>
@@ -158,7 +147,7 @@ var pagename =  window.location.href.split("/").slice(-1);
 $('#navbar a[href="'+pagename+'"]').parent().addClass('active');
 
 //get position of chosen lc class among accordion headers so we know which one to expand
-   var curclass=	$("#<?php echo substr($lcclass,0,1) ?>class").parent().children('h3').index($('#<?php echo substr($lcclass,0,1) ?>class'));
+var curclass = $("#<?php echo substr($lcclass, 0, 1); ?>class").parent().children('h3').index($('#<?php echo substr($lcclass, 0, 1); ?>class'));
 if (curclass == -1){
 var findclass = "false";	
 }
