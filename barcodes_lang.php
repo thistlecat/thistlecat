c67131cd9fb55fcd8602b227a27f495acbe1d275
maderikra <6333132+maderikra@users.noreply.github.com>
2017-08-14 13:38:49 -0400

    formatting

--- barcodes_lang.php
+++ barcodes_lang.php
@@ -352,11 +352,7 @@ display_chart("Copyright Date");
 
 $(document).ready(function(e) {
 //get position of chosen lc class among accordion headers so we know which one to expand
-   var curclass=	$("#<?php
-echo substr($thisauthor, 0, 1);
-?>class").parent().children('h3').index($('#<?php
-echo substr($thisauthor, 0, 1);
-?>class'));
+   var curclass = $("#<?php echo substr($thisauthor, 0, 1); ?>class").parent().children('h3').index($('#<?php echo substr($thisauthor, 0, 1); ?>class'));
 if (curclass == -1){
 var findclass = "false";	
 }
