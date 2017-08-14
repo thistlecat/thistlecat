c7a1d8c1278d481e7016ae903fb2e63883a61c66
maderikra <6333132+maderikra@users.noreply.github.com>
2017-08-14 13:55:10 -0400

    formatting

--- itemlookup.php
+++ itemlookup.php
@@ -1,8 +1,4 @@
 
-<script type="text/javascript" src="js/jquery.tablesorter.min.js"></script> 
-
-
-
 <style type="text/css">
 .headerbuttons {
     float: right;
@@ -31,19 +27,15 @@ $("#closerecord").click(function() {
             <table class="table table-condensed tablesorter" id="resultstab">
 
 <?php
- 
-
-
 $barcode = $_GET["barcode"];
 
-	
 include "includes/dbconnect.php";
 
-
 $sql0 = 'SELECT title,itemcallnumber,cn_sort,issues,copyrightdate,barcode,oclc,biblionumber,lastborrowed,special,duplicates FROM ' . THIS_TABLE . ' WHERE barcode = :barcode';
-
 $stmt = $pdo->prepare($sql0);
-$stmt->execute( array(':barcode' => $barcode ) );
+$stmt->execute(array(
+    ':barcode' => $barcode
+));
 $searchresults = $stmt->fetch();
 
 
@@ -51,37 +43,29 @@ echo "<tr class='active'><td><h3>";
 
 //insert first catalog link, if there is one
 if ($catalogs[0]['field'] <> "") {
-$fixedurl = str_replace("MAGICNUMBER", $searchresults[$catalogs[0]['field']], $catalogs[0]['pattern']);		
-echo "<a target='_blank' href='" . $fixedurl . "'>";
-echo $searchresults['title'];
-echo "</a>";
-
-//check if there are multiple catalog entries
-if (count($catalogs) > 1){
-	
-	    //copy original array so we can shift it (remove first element)
-		$shiftedarray = $catalogs;
-		array_shift($shiftedarray);
-		foreach ($shiftedarray as $catval){
-			
-		
-		$fixedurl = str_replace("MAGICNUMBER", $searchresults[$catval['field']], $catval['pattern']);		
-		echo " <a target='_blank' href='" . $fixedurl . "'>[" . $catval['abbrev'] . "]</a>";
-		}
+    $fixedurl = str_replace("MAGICNUMBER", $searchresults[$catalogs[0]['field']], $catalogs[0]['pattern']);
+    echo "<a target='_blank' href='" . $fixedurl . "'>";
+    echo $searchresults['title'];
+    echo "</a>";
+    
+    //check if there are multiple catalog entries
+    if (count($catalogs) > 1) {
+        
+        //copy original array so we can shift it (remove first element)
+        $shiftedarray = $catalogs;
+        array_shift($shiftedarray);
+        foreach ($shiftedarray as $catval) {
+            $fixedurl = str_replace("MAGICNUMBER", $searchresults[$catval['field']], $catval['pattern']);
+            echo " <a target='_blank' href='" . $fixedurl . "'>[" . $catval['abbrev'] . "]</a>";
+        }
+    }
+    
+} else {
+    echo $searchresults['title'];
 }
 
-}
-else{
-echo $searchresults['title'];
-	
-	
-}
-		   
 echo "</tr>";
 echo "<tr ><td> <div class='row'>";
-   
-      
-    
 echo '<div class="col-md-6"><dl class="dl-horizontal">';
 echo '<dt>Copyright Date:</dt>';
 echo '<dd>' . $searchresults['copyrightdate'] . '</dd>';
@@ -90,7 +74,7 @@ echo '<dd>' . $searchresults['itemcallnumber'] . '</dd>';
 echo '<dt>Total Checkouts:</dt>';
 echo '<dd>' . $searchresults['issues'] . '</dd>';
 echo '<dt>Last Checkout Date:</dt>';
-echo '<dd>' . substr($searchresults['lastborrowed'],0,10) . '</dd>';
+echo '<dd>' . substr($searchresults['lastborrowed'], 0, 10) . '</dd>';
 echo "</dl>";
 echo '</div><div class="col-md-6"><dl class="dl-horizontal">';
 echo '<dt>Item Barcode:</dt>';
@@ -99,17 +83,14 @@ echo '<dt>OCLC Number:</dt>';
 echo '<dd>' . $searchresults['oclc'] . '</dd>';
 
 if ($searchresults['duplicates']) {
-echo '<dt>Possible Duplicates:</dt><dd>'; 
-//parse dups list	
-$alldups = explode(",", $searchresults['duplicates']);
-foreach ($alldups as $dupe){
-echo ' <a target="_blank" href="http://vmi.worldcat.org/oclc/' . $dupe . '"><span class="label label-danger">' . $dupe . '</span></a>';
+    echo '<dt>Possible Duplicates:</dt><dd>';
+    //parse dups list	
+    $alldups = explode(",", $searchresults['duplicates']);
+    foreach ($alldups as $dupe) {
+        echo ' <a target="_blank" href="http://vmi.worldcat.org/oclc/' . $dupe . '"><span class="label label-danger">' . $dupe . '</span></a>';
+    }
+    echo '</dd>';
 }
-echo  '</dd>';
-}
-
-
-
 
 echo "</dl></div></td></tr>";
 
@@ -117,21 +98,14 @@ echo "<tr class='bg-success'><td>";
 echo '<dl class="dl-horizontal">';
 echo '<dt>Special Attributes:</dt>';
 echo '<dd>';
-if ($searchresults['special'] <> ""){echo ' <span class="label label-success">' . $searchresults['special'] . '</span>';}
+if ($searchresults['special'] <> "") {
+    echo ' <span class="label label-success">' . $searchresults['special'] . '</span>';
+}
 echo '</dd>';
 echo '<dd>';
 echo '<span style="float:right;"><button type="button" class="btn btn-danger btn-sm" id="closerecord"><i class="fa fa-times" aria-hidden="true"></i> Close</span>';
 echo '</dd>';
 echo "</dl>";
 echo "</td></tr>";
-
-
-
-
-
 ?>
-
-
 </table>
-    
-<!-- <a class="btn btn-warning btn-sm" href="index.php" role="button">&laquo;Go back </a><br /><br /> -->
