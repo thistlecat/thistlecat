236e3b1aa61505a19a223eaf501f26d6a60f7f5f
maderikra <6333132+maderikra@users.noreply.github.com>
2017-08-15 12:03:16 -0400

    rm dropdown from page with no charts

--- querybuilder.php
+++ querybuilder.php
@@ -137,22 +137,7 @@ th { cursor: pointer; }
 echo $libraryname;
 ?></a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-       
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Select Layer... <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="checkouts.php">Total Checkouts</a></li>
-                <li><a href="lastborrowed.php">Last Checkout Date</a></li>
-                
-                <li><a href="#">U.S. Holdings</a></li>
-               
-              </ul>
-            </li>
-          </ul>
-        
-        </div>
+  
       </div>
     </nav>
 
